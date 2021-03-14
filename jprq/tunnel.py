6ce028e1e3e75459b65a6cce8cd21fb2aa3e4703
Azimjon Pulatov <gmail@azimjon.com>
2021-03-14 11:02:22 +0100

    Add Error Handling

--- jprq/tunnel.py
+++ jprq/tunnel.py
@@ -1,4 +1,6 @@
 import asyncio
+import sys
+
 import bson
 import websockets
 import ssl
@@ -13,6 +15,13 @@ ssl_context.load_verify_locations(certifi.where())
 async def open_tunnel(ws_uri: str, http_uri):
     async with websockets.connect(ws_uri, ssl=ssl_context) as websocket:
         message = bson.loads(await websocket.recv())
+
+        if message.get("warning"):
+            print(message["warning"], file=sys.stderr)
+
+        if message.get("error"):
+            print(message["error"], file=sys.stderr)
+            return
 
         host, token = message["host"], message["token"]
         print(f"Online at https://{host}/")
