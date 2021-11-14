ed0bea37119669141eaf22a22837880ead9900be
Azimjon Pulatov <azimjohn@yahoo.com>
2021-11-14 22:50:55 +0100

    Show IPs for TCP Connections

--- jprq_cli/client/tunnel_tcp.go
+++ jprq_cli/client/tunnel_tcp.go
@@ -18,7 +18,8 @@ type TCPTunnel struct {
 }
 
 type ConnectionRequest struct {
-	Flag int `json:"public_client_port"`
+	IP   string `json:"public_client_ip"`
+	Flag int    `json:"public_client_port"`
 }
 
 func openTCPTunnel(port int, ctx context.Context) {
@@ -44,7 +45,7 @@ func openTCPTunnel(port int, ctx context.Context) {
 	fmt.Printf("Forwarded:\t\t%s:%d → 127.0.0.1:%d\n\n",
 		tcpBaseHost, tunnel.PublicServerPort, port)
 
-	connRequests := make(chan int)
+	connRequests := make(chan ConnectionRequest)
 	defer close(connRequests)
 
 	go handleTCPConnections(ws, connRequests)
@@ -54,15 +55,15 @@ func openTCPTunnel(port int, ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			break out
-		case flag := <-connRequests:
-			go handleTCPConnection(flag, port, tunnel.PrivateServerPort, ctx)
+		case connRequest := <-connRequests:
+			go handleTCPConnection(connRequest, port, tunnel.PrivateServerPort, ctx)
 		}
 	}
 
 	fmt.Println("\n\033[31mjprq tunnel closed\033[00m")
 }
 
-func handleTCPConnections(ws *websocket.Conn, connRequests chan<- int) {
+func handleTCPConnections(ws *websocket.Conn, connRequests chan<- ConnectionRequest) {
 	for {
 		var connReq ConnectionRequest
 		_, message, err := ws.ReadMessage()
@@ -73,13 +74,13 @@ func handleTCPConnections(ws *websocket.Conn, connRequests chan<- int) {
 		if err != nil {
 			log.Printf("Error Decoding Message %s\n", message)
 		}
-		connRequests <- connReq.Flag
+		connRequests <- connReq
 	}
 }
 
-func handleTCPConnection(flag, localServerPort, remoteServerPort int, ctx context.Context) {
-	fmt.Println("New Connection +1")
-	defer fmt.Println("Connection Closed")
+func handleTCPConnection(connRequest ConnectionRequest, localServerPort, remoteServerPort int, ctx context.Context) {
+	fmt.Printf("> Opened Connection with %s\n", connRequest.IP)
+	defer fmt.Printf("> Closed Connection with %s\n", connRequest.IP)
 
 	localConn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", localServerPort))
 	if err != nil {
@@ -95,6 +96,7 @@ func handleTCPConnection(flag, localServerPort, remoteServerPort int, ctx contex
 	}
 	defer remoteConn.Close()
 
+	flag := connRequest.Flag
 	_, err = remoteConn.Write([]byte{byte(flag >> 8 & 0xFF), byte(flag & 0xFF)})
 	if err != nil {
 		fmt.Printf("Error Sending Data to Remote Server: %s\n", err.Error())
