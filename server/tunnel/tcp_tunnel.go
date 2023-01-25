758de9b291aeb344e602d29d4d083fd96585f480
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2023-01-25 02:02:27 +0100

    Implement TCPTunnel - incoplete Start()

--- server/tunnel/tcp_tunnel.go
+++ server/tunnel/tcp_tunnel.go
@@ -1,1 +1,47 @@
 package tunnel
+
+import (
+	"fmt"
+	"github.com/azimjohn/jprq/server/server"
+)
+
+type TCPTunnel struct {
+	hostname      string
+	publicServer  server.TCPServer
+	privateServer server.TCPServer
+}
+
+func NewTCPTunnel(hostname string) (*TCPTunnel, error) {
+	var t TCPTunnel
+	t.hostname = hostname
+	if err := t.privateServer.Init(0); err != nil {
+		return nil, fmt.Errorf("error init private server: %w", err)
+	}
+	if err := t.publicServer.Init(0); err != nil {
+		return nil, fmt.Errorf("error init public server: %w", err)
+	}
+	return &t, nil
+}
+
+func (t *TCPTunnel) Protocol() string {
+	return "tcp"
+}
+
+func (t *TCPTunnel) Hostname() string {
+	return t.hostname
+}
+
+func (t *TCPTunnel) PublicServerPort() uint16 {
+	return t.publicServer.Port()
+}
+
+func (t *TCPTunnel) PrivateServerPort() uint16 {
+	return t.privateServer.Port()
+}
+
+func (t *TCPTunnel) Start() {
+	go t.privateServer.Start()
+	go t.publicServer.Start()
+
+	// handle private and public servers
+}
