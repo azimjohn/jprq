0f7d603cfedfe5d8366b4cce67f8b5b3b1974de4
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2023-01-25 01:55:39 +0100

    Add DomainName and MaxConsPerTunnel to Config

--- server/config/config.go
+++ server/config/config.go
@@ -6,6 +6,8 @@ import (
 )
 
 type Config struct {
+	DomainName          string
+	MaxConsPerTunnel    uint16
 	EventServerPort     uint16
 	PublicServerPort    uint16
 	EventServerTLSPort  uint16
@@ -15,12 +17,18 @@ type Config struct {
 }
 
 func (c *Config) Load() error {
+	c.MaxConsPerTunnel = 50
 	c.PublicServerPort = 80
 	c.EventServerPort = 4321
 	c.EventServerTLSPort = 4322
 	c.PublicServerTLSPort = 443
-	c.TLSKeyFile = os.Getenv("TLS_KEY_FILE")
-	c.TLSCertFile = os.Getenv("TLS_CERT_FILE")
+	c.DomainName = os.Getenv("JPRQ_DOMAIN")
+	c.TLSKeyFile = os.Getenv("JPRQ_TLS_KEY")
+	c.TLSCertFile = os.Getenv("JPRQ_TLS_CERT")
+
+	if c.DomainName == "" {
+		return errors.New("JPRQ_DOMAIN env is not set")
+	}
 
 	if c.TLSKeyFile == "" || c.TLSCertFile == "" {
 		return errors.New("TLS key/cert file is missing")
