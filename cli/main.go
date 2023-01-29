4c3e68537e72420320d24b7a2f798b7775f34603
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2023-01-29 18:03:10 +0100

    change file mode to LF

--- cli/main.go
+++ cli/main.go
@@ -1,1 +1,38 @@
-package mainimport (	"log"	"time")type Request struct {	Id      uint64            `json:"id"`	Method  string            `json:"method"`	URL     string            `json:"url"`	Body    string            `json:"body"`	Headers map[string]string `json:"header"`}type Response struct {	RequestId uint64            `json:"request_id"`	Status    int               `json:"status"`	Headers   map[string]string `json:"header"`	Body      string            `json:"body"`}func main() {	w := NewWebServer()	go func() {		r := Request{}		ticker := time.NewTicker(1 * time.Second)		for _ = range ticker.C {			w.DispatchEvent(r)		}	}()	if err := w.Run(4444); err != nil {		log.Fatalf("fail to run web server: %v", err)	}}
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type Request struct {
+	Id      uint64            `json:"id"`
+	Method  string            `json:"method"`
+	URL     string            `json:"url"`
+	Body    string            `json:"body"`
+	Headers map[string]string `json:"header"`
+}
+
+type Response struct {
+	RequestId uint64            `json:"request_id"`
+	Status    int               `json:"status"`
+	Headers   map[string]string `json:"header"`
+	Body      string            `json:"body"`
+}
+
+func main() {
+	w := NewWebServer()
+
+	go func() {
+		r := Request{}
+
+		ticker := time.NewTicker(1 * time.Second)
+		for _ = range ticker.C {
+			w.DispatchEvent(r)
+		}
+	}()
+
+	if err := w.Run(4444); err != nil {
+		log.Fatalf("fail to run web server: %v", err)
+	}
+}
