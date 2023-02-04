e477062760f8d9de77e75711f3b4ab7f6bf4df57
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2023-02-04 21:28:59 +0100

    [server] Add Read/Write Support for Events

--- server/events/event_test.go
+++ server/events/event_test.go
@@ -1,6 +1,7 @@
 package events
 
 import (
+	"io"
 	"testing"
 )
 
@@ -22,6 +23,38 @@ func TestEvent_EncodeDecode(t *testing.T) {
 	err = result.Decode(data)
 	if err != nil {
 		t.Errorf("error decoding %v", err)
+	}
+
+	if result.Data.ClientIP != event.Data.ClientIP {
+		t.Logf("expected %s, got %s", event.Data.ClientIP, result.Data.ClientIP)
+		t.Fail()
+	}
+
+	if result.Data.ClientPort != event.Data.ClientPort {
+		t.Logf("expected %d, got %d", event.Data.ClientPort, result.Data.ClientPort)
+		t.Fail()
+	}
+}
+
+func TestEvent_ReadWrite(t *testing.T) {
+	pr, pw := io.Pipe()
+	event := Event[ConnectionReceived]{
+		Data: &ConnectionReceived{
+			ClientIP:    "127.0.0.1",
+			ClientPort:  8000,
+			RateLimited: false,
+		},
+	}
+
+	go func() {
+		if err := event.Write(pw); err != nil {
+			t.Errorf("error writing: %v", err)
+		}
+	}()
+
+	var result Event[ConnectionReceived]
+	if err := result.Read(pr); err != nil {
+		t.Errorf("error reading: %v", err)
 	}
 
 	if result.Data.ClientIP != event.Data.ClientIP {
--- server/events/events.go
+++ server/events/events.go
@@ -2,7 +2,9 @@ package events
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/gob"
+	"io"
 )
 
 type Protocol string
@@ -18,25 +20,6 @@ type EventType interface {
 
 type Event[Type EventType] struct {
 	Data *Type
-}
-
-func (e *Event[EventType]) Encode() ([]byte, error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	if err := enc.Encode(e.Data); err != nil {
-		return nil, err
-	}
-	data := buf.Bytes()
-	return data, nil
-}
-
-func (e *Event[EventType]) Decode(data []byte) error {
-	buf := bytes.NewBuffer(data)
-	dec := gob.NewDecoder(buf)
-	if err := dec.Decode(&e.Data); err != nil {
-		return err
-	}
-	return nil
 }
 
 type TunnelRequested struct {
@@ -59,4 +42,55 @@ type ConnectionReceived struct {
 	ClientIP    string `json:"client_ip"`
 	ClientPort  uint16 `json:"client_port"`
 	RateLimited bool   `json:"rate_limited"`
+}
+
+func (e *Event[EventType]) Encode() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(e.Data); err != nil {
+		return nil, err
+	}
+	data := buf.Bytes()
+	return data, nil
+}
+
+func (e *Event[EventType]) Decode(data []byte) error {
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+	if err := dec.Decode(&e.Data); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Event[EventType]) Read(conn io.Reader) error {
+	buffer := make([]byte, 2)
+	if _, err := conn.Read(buffer); err != nil {
+		return err
+	}
+	length := binary.LittleEndian.Uint16(buffer)
+	buffer = make([]byte, length)
+	if _, err := conn.Read(buffer); err != nil {
+		return err
+	}
+	if err := e.Decode(buffer); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Event[EventType]) Write(conn io.Writer) error {
+	data, err := e.Encode()
+	if err != nil {
+		return err
+	}
+	length := make([]byte, 2)
+	binary.LittleEndian.PutUint16(length, uint16(len(data)))
+	if _, err := conn.Write(length); err != nil {
+		return err
+	}
+	if _, err := conn.Write(data); err != nil {
+		return err
+	}
+	return nil
 }
