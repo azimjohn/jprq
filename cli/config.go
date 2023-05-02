2f1d8a60a3104e443c82a4c2f32e8fc45f47cb13
Kamron Mirsamatov <kronusk2@gmail.com>
2023-05-02 06:26:07 +0200

    in case XDG_CONFIG_HOME not set up

--- cli/config.go
+++ cli/config.go
@@ -56,7 +56,7 @@ func (c *Config) Write() error {
 		return fmt.Errorf("error getting user config directory: %s", err)
 	}
 	dirPath := filepath.Join(configDir, "jprq")
-    if err := os.Mkdir(dirPath, 0700); err != nil && os.IsNotExist(err) {
+    if err := os.MkdirAll(dirPath, 0700); err != nil && os.IsNotExist(err) {
 		return fmt.Errorf("error creating config directory: %s", err)
     }
     filePath := filepath.Join(dirPath, localConfig)
