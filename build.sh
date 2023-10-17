7a60d43a03ac73ed21672a895d6e6028cf3a588e
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2023-10-17 12:26:19 +0200

    optimize builds

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-GOOS=darwin     GOARCH=amd64    go build -o bin/jprq-darwin-amd64       cli/*.go
-GOOS=darwin     GOARCH=arm64    go build -o bin/jprq-darwin-arm64       cli/*.go
-GOOS=linux      GOARCH=386      go build -o bin/jprq-linux-386          cli/*.go
-GOOS=linux      GOARCH=amd64    go build -o bin/jprq-linux-amd64        cli/*.go
-GOOS=linux      GOARCH=arm      go build -o bin/jprq-linux-arm          cli/*.go
-GOOS=linux      GOARCH=arm64    go build -o bin/jprq-linux-arm64        cli/*.go
-GOOS=windows    GOARCH=386      go build -o bin/jprq-windows-386.exe    cli/*.go
-GOOS=windows    GOARCH=amd64    go build -o bin/jprq-windows-amd64.exe  cli/*.go
+GOOS=darwin     GOARCH=amd64    go build -ldflags '-s' -o bin/jprq-darwin-amd64       cli/*.go
+GOOS=darwin     GOARCH=arm64    go build -ldflags '-s' -o bin/jprq-darwin-arm64       cli/*.go
+GOOS=linux      GOARCH=386      go build -ldflags '-s' -o bin/jprq-linux-386          cli/*.go
+GOOS=linux      GOARCH=amd64    go build -ldflags '-s' -o bin/jprq-linux-amd64        cli/*.go
+GOOS=linux      GOARCH=arm      go build -ldflags '-s' -o bin/jprq-linux-arm          cli/*.go
+GOOS=linux      GOARCH=arm64    go build -ldflags '-s' -o bin/jprq-linux-arm64        cli/*.go
+GOOS=windows    GOARCH=386      go build -ldflags '-s' -o bin/jprq-windows-386.exe    cli/*.go
+GOOS=windows    GOARCH=amd64    go build -ldflags '-s' -o bin/jprq-windows-amd64.exe  cli/*.go
